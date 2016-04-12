b97f7ca6484db5cd5f14908ad1c44c756fb2657b
cubecnelson <cubecnelson@gmail.com>
2016-04-12 17:17:22 +0800

    sentiment

--- tweet_sentiment_by_pattern.py
+++ tweet_sentiment_by_pattern.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+import pymongo
 from pattern.en import sentiment
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,40 +8,56 @@ import re
 
 db = MongoClient().test_database
 
-count = 0.0
-data = {}
-
-QUERY = ''
-
-
-
-for tweet in db.tweets.find():
-	myText = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', tweet["text"].replace("#", "").replace("@", ""))
-	#print tweet["favorite_count"]
-	response = math.ceil(sentiment(myText.replace("#", ""))[0]*20)/20
-	if response != 0.0:
-		for hashtag in tweet["entities"]["hashtags"]:
-			#print hashtag['text'].encode("utf-8")
-			if QUERY.lower() in hashtag['text'].lower():
-				try:
-					data[response] = data[response] + 1.0
-				except KeyError:
-					data[response] = 1.0
-				count = count + 1.0
-				break
-			elif QUERY.lower() in tweet['text'].lower():
-				try:
-					data[response] = data[response] + 1.0
-				except KeyError:
-					data[response] = 1.0
-				count = count + 1.0
-				break
-
+for trend in db.trends.find():
+	count = 0.0
 	
-for key in data.keys():
-	data[key] = data[key]/count
+	data = {}
+
+	print trend['_id']
+	QUERY = trend['_id']
+
+	QUERY = QUERY.lower()
+
+	record = []
+
+	zeros = np.linspace(-1,1,40)
 
 
-plt.title("\"" + QUERY.upper() + "\""  + ": SENTIMENT - #TWEETS")
-plt.scatter(data.keys(), data.values())
-plt.show()
+	for zero in zeros:
+		try:
+			zero = math.ceil(zero*20)/20
+			data[zero] = 0
+		except KeyError:
+			data[zero] = 0
+			break
+
+
+	#plt.show()
+	for tweet in db.tweets.find({"entities.hashtags.text": re.compile(QUERY, re.IGNORECASE)}):
+		myText = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', tweet["text"].replace("#", "").replace("@", ""))
+		#print tweet["favorite_count"]
+		response = math.ceil(sentiment(myText.replace("#", ""))[0]*20)/20
+		if response != 0.0:
+				#print hashtag['text'].encode("utf-8")
+			record.append(response)
+			count = count + 1.0
+			try:
+				data[response] = data[response] + 1.0
+			except KeyError:
+				data[response] = 1.0
+				
+	print data
+		
+	if count != 0:
+		for key in data.keys():
+			data[key] = data[key]/count
+
+	values = data.values();
+	with open("hashtags_sentiment.txt", "a") as text_file:
+		for value in values:
+			text_file.write(str(value) + ",")
+		text_file.write(str(count) + "\n\n")
+
+	#plt.title("\"" + QUERY.upper() + "\""  + ": SENTIMENT - #TWEETS")
+	#plt.scatter(data.keys(), data.values())
+	#plt.show()
